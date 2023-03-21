a6cbad7db8d40ee0e94cd37dba881e9625551d48
Ronaldo-Severo <roneros@gmail.com>
2023-03-20 21:02:57 -0300

    message test soma

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,9 +6,9 @@ public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
 		
-		/*int  a = 2;
+		int  a = 2;
 		int  b = 3;
-		System.out.println("Hello World! " + (a+b)); */
+		System.out.println("Hello World! " + (a+b)); 
 		
 		Gato gato = new Gato(null, null, null);
 		Livros livros = new Livros();
